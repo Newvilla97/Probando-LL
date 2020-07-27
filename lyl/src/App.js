0568bef05f5730ad7f19049e6f1015d7c18e7fde
Newvilla97 <villanuevaleo@outlook.com>
2020-07-27 20:24:39 -0300

    hola señor bush

--- lyl/src/App.js
+++ lyl/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Bienvenidos muchachos
+         hola hola señor bush!!!
         </p>
         <a
           className="App-link"

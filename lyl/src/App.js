e9eb26cd9199521571b0873bc62532619a871cb0
Newvilla97 <villanuevaleo@outlook.com>
2020-07-27 20:28:29 -0300

    asdasd

--- lyl/src/App.js
+++ lyl/src/App.js
@@ -8,12 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Bienvenidos muchachos
-         hola
-         hola
-         hola
-         hola
-         hola
+        eres un soquete
          
         </p>
         <a
